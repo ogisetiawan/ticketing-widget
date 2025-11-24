5bfec0379f772e6611cb4dbd60f413c5d5b522f9
ogisetiawan <ogisetiawan21@gmail.com>
2025-11-24 16:39:29 +0700

    ✨feat: update Vite configuration and add test HTML page for ticketing widget

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,39 @@
+// vite.config.js
 import { defineConfig } from "vite";
+import { resolve } from 'path';
 
 export default defineConfig({
   server: {
-    port: 5173,
+    port: 5175,
     open: true,
   },
   optimizeDeps: {
     include: ["lottie-web"],
   },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'BMTicketingWidget',
+      fileName: 'bm-ticketing-widget',
+      formats: ['iife'],
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'bm-ticketing-widget.css';
+          }
+          return assetInfo.name;
+        },
+      },
+      onwarn(warning, warn) {
+        if (warning.code === 'EVAL' && warning.id.includes('lottie-web')) {
+          return;
+        }
+        warn(warning);
+      },
+    },
+    cssCodeSplit: false,
+  },
+  publicDir: 'public',
 });
-
